e954857611312dab7458d4cdf04cc02baa70e3c6
Alex Kallam <aakallam@vt.edu>
2013-12-04 16:11:45 -0500

    Added MemoryManager class

---
+++ MemoryManager.java
@@ -0,0 +1,16 @@
+
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ *
+ *  @author Alex
+ *  @version Dec 4, 2013
+ */
+public class MemoryManager
+{
+    public MemoryManager() {
+
+    }
+}
