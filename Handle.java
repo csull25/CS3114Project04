87fcc6aa9713e270355df1d18cf678c6d9542185
Alex Kallam <akdrummer@gmail.com>
2013-12-04 19:47:54 -0500

    Updated Handle functionality

--- Handle.java
+++ Handle.java
@@ -1,7 +1,8 @@
 
 // -------------------------------------------------------------------------
 /**
- *  Pointer for data in Memory Manager
+ *  Pointer for data in Memory Manager. Allows data to be moved (which updates
+ *  position value) without messing up top-level reference.
  *
  *  @author Alex Kallam
  *  @author Connor J. Sullivan
@@ -9,11 +10,32 @@
  */
 public class Handle
 {
+    /**  */
+    int position;
     // ----------------------------------------------------------
     /**
      * Create a new Handle object.
+     * @param pos byte position of object
      */
-    public Handle() {
+    public Handle(int pos) {
+        this.position = pos;
+    }
 
+    // ----------------------------------------------------------
+    /**
+     * Set the position the handle points to
+     * @param pos byte position of data
+     */
+    public void setPosition(int pos) {
+        this.position = pos;
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Get the position of the handle
+     * @return position handle points to
+     */
+    public int getPosition() {
+        return this.position;
     }
 }
