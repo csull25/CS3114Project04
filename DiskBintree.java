a182f7d655e3522a8657dbf074982ec5702ab711
csull25 <csull@vt.edu>
2013-12-09 12:37:42 -0500

    added files.. Adding search functionality to diskbintree

--- DiskBintree.java
+++ DiskBintree.java
@@ -86,6 +86,7 @@ public class DiskBintree
             {
                 debugRequest();
             }
+            else if (command.contains("search"));
         }
         commandFile.close();
     }
